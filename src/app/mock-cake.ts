3931b8fb2623d1100730ec85a5e3dc5a14206ded
sqz777 <sqz777isme@gmail.com>
2018-06-29 00:00:21 +0800

    replace picUrl

--- src/app/mock-cake.ts
+++ src/app/mock-cake.ts
@@ -1,6 +1,6 @@
-import {Cake} from './cake'
+import {Cake} from './cake';
 export const CAKES:Cake[]=[
-    {id: 1, name: 'Chocolate Cake', picUrl: ''},
+    {id: 1, name: 'Chocolate Cake', picUrl: "chocolate cake.jpg"},
     {id: 2, name: 'Cheese Cake', picUrl: ''},
     {id: 3, name: 'Mongo Cake', picUrl: ''},
     {id: 4, name: 'Mocha Cake', picUrl: ''},
