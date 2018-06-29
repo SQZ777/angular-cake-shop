9b51db8d09f6b9dd673539777218b20ae5c0f878
Darren,Zhang [DZ] <sqz777isme@gmail.com>
2018-06-29 13:53:16 +0800

    rename imgs and mock data

--- src/app/mock-cake.ts
+++ src/app/mock-cake.ts
@@ -1,9 +1,9 @@
 import {Cake} from './cake';
 export const CAKES:Cake[]=[
-    {id: 1, name: 'Chocolate Cake', picUrl: "chocolate cake.jpg"},
-    {id: 2, name: 'Cheese Cake', picUrl: "cheese cake.jpg"},
-    {id: 3, name: 'Mongo Cake', picUrl: "mongo cake.jpg"},
-    {id: 4, name: 'Mocha Cake', picUrl: "mocha cake.jpg"},
-    {id: 5, name: 'Strawberry Cake', picUrl: "strawberry cake.jpg"},
+    {id: 1, name: 'Chocolate Cake', picUrl: "chocolateCake.jpg"},
+    {id: 2, name: 'Cheese Cake', picUrl: "cheeseCake.jpg"},
+    {id: 3, name: 'Mongo Cake', picUrl: "mongoCake.jpg"},
+    {id: 4, name: 'Mocha Cake', picUrl: "mochaCake.jpg"},
+    {id: 5, name: 'Strawberry Cake', picUrl: "strawberryCake.jpg"},
 
 ]
