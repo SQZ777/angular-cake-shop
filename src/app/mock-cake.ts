4f2e61ce533644c9bbe2dde03e838ced6f93bfef
sqz777 <sqz777isme@gmail.com>
2018-07-19 22:35:15 +0800

    fix format

--- src/app/mock-cake.ts
+++ src/app/mock-cake.ts
@@ -1,9 +1,8 @@
-import {Cake} from './cake';
-export const CAKES:Cake[]=[
-    {id: 1, name: 'Chocolate Cake', picUrl: "chocolateCake.jpg"},
-    {id: 2, name: 'Cheese Cake', picUrl: "cheeseCake.jpg"},
-    {id: 3, name: 'Mongo Cake', picUrl: "mongoCake.jpg"},
-    {id: 4, name: 'Mocha Cake', picUrl: "mochaCake.jpg"},
-    {id: 5, name: 'Strawberry Cake', picUrl: "strawberryCake.jpg"},
-
-]
+import { Cake } from './cake';
+export const CAKES: Cake[] = [
+    { id: 1, name: 'Chocolate Cake', picUrl: 'chocolateCake.jpg' },
+    { id: 2, name: 'Cheese Cake', picUrl: 'cheeseCake.jpg' },
+    { id: 3, name: 'Mongo Cake', picUrl: 'mongoCake.jpg' },
+    { id: 4, name: 'Mocha Cake', picUrl: 'mochaCake.jpg' },
+    { id: 5, name: 'Strawberry Cake', picUrl: 'strawberryCake.jpg' },
+];
