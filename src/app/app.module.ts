10ba0b7b16c687e9d2de41546a95411c1445dbc8
sqz777 <sqz777isme@gmail.com>
2018-07-19 21:01:53 +0800

    add a router

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { CakesComponent } from './cakes/cakes.component';
 import { CakeDetailComponent } from './cake-detail/cake-detail.component';
 import { MessagesComponent } from './messages/messages.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { MessagesComponent } from './messages/messages.component';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
