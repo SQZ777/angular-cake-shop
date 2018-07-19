c6ca2efef3933d575ed2918b0bfec31ad8bcece6
sqz777 <sqz777isme@gmail.com>
2018-07-19 21:53:52 +0800

    router complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
+import { CakesComponent } from './cakes/cakes.component';
+
+const routes: Routes = [
+  { path: 'cakes', component: CakesComponent }
+];
 
 @NgModule({
+  imports: [ RouterModule.forRoot(routes) ],
   exports: [
     RouterModule
   ]
 })
+
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,13 +7,15 @@ import { CakesComponent } from './cakes/cakes.component';
 import { CakeDetailComponent } from './cake-detail/cake-detail.component';
 import { MessagesComponent } from './messages/messages.component';
 import { AppRoutingModule } from './/app-routing.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CakesComponent,
     CakeDetailComponent,
-    MessagesComponent
+    MessagesComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
